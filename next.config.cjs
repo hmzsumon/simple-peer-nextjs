b58cb47a77b712258cb4e4c016a71bcb84990477
hmzsumon <zsumonn@gmail.com>
2025-03-26 02:28:48 +0600

    Initial commit: WebRTC live app with mic toggle

--- next.config.cjs
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const { withNetlify } = require('@netlify/next');
-
-module.exports = withNetlify({
-	experimental: {
-		esmExternals: true,
-	},
-});
